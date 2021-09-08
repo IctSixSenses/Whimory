a7b35649f6e2894468326f4175ea593e0f919bd5
SYSY3838 <85145445+SYSY3838@users.noreply.github.com>
2021-09-08 22:56:58 +0900

    Revert "Hyun joo"

--- src/main/java/com/test/whimory/user/model/service/UserService.java
+++ src/main/java/com/test/whimory/user/model/service/UserService.java
@@ -1,10 +1,5 @@
 package com.test.whimory.user.model.service;
 
-import com.test.whimory.user.model.vo.User;
-
-public interface UserService {
-	//
-	User selectUser(String user_id);//회원 정보 조회하기
-	User selectLogin(User user); //로그인 관련 메소드
+public class UserService {
 
 }
