15f31e378c4cce73f33fb4893761f5aa5441194b
hyunjoo <hjpark1397@naver.com>
2021-09-30 13:39:27 +0900

    userController- kakao관련삭제

--- src/main/java/com/test/whimory/user/controller/UserController.java
+++ src/main/java/com/test/whimory/user/controller/UserController.java
@@ -41,7 +41,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.test.whimory.user.model.service.UserService;
-import com.test.whimory.user.model.vo.KakaoLoginOutput;
+
 import com.test.whimory.user.model.vo.User;
 import com.test.whimory.user.model.vo.UserDrop;
 
